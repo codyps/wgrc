92f12b812eca0ffc7cac4f1ea505af15e152dec9
Cody P Schafer <dev@codyps.com>
2020-05-23 02:37:00 -0400

    somewhat functional

--- Makefile
+++ Makefile
@@ -9,5 +9,4 @@ build/cmd: proto
 
 .PHONY: proto
 proto:
-	#protoc api/stream.proto -I. --go_out=plugins=grpc:$(GOPATH)/src
 	protoc api/stream.proto -I. --go_out=plugins=grpc:. --go_opt=paths=source_relative
--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,40 @@
 //go:generate protoc --go_out=plugins=grpc:api stream.proto
 package main
+
 import (
 	"log"
 	"net"
+	//"context"
 
-	"github.com/jmesmon/wgrc/api"
+	pb "github.com/jmesmon/wgrc/api"
+	"google.golang.org/grpc"
 )
 
+type streamerServer struct {
+	pb.UnimplementedStreamerServer
+}
+
+func (s *streamerServer) Listen(in *pb.ListenReq, stream pb.Streamer_ListenServer) error {
+	stream.Send(&pb.ListenEvent{})
+	stream.Send(&pb.ListenEvent{ Other: "sssss"})
+	return nil
+}
+
 func main() {
-	fmt.Println("hi")
+
+	listen, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	srv := streamerServer{}
+
+	pb.RegisterStreamerServer(s, &srv)
+
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to server: %s", err)
+	}
+
 }
