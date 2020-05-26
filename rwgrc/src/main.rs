bb72ae885b046221b8d8d5505a305e396b79ca62
Cody P Schafer <dev@codyps.com>
2020-05-26 03:43:21 -0400

    throttle output

--- rwgrc/src/main.rs
+++ rwgrc/src/main.rs
@@ -1,6 +1,7 @@
 use tonic::{transport::Server, Request, Response, Status};
 use rand::prelude::*;
 
+use log::info;
 use tokio::sync::mpsc;
 use tokio::sync::Mutex;
 use std::sync::Arc;
@@ -64,12 +65,11 @@ pub struct StreamX {
 impl Streamer for StreamX {
     type ListenStream = mpsc::Receiver<Result<ListenEvent, Status>>;
 
-
     async fn listen(
         &self,
         _request: Request<ListenReq>,
     ) -> Result<Response<Self::ListenStream>, Status> {
-        println!("streaming!");
+        //println!("streaming!");
         let (mut tx, rx) = mpsc::channel(4);
         let ss = self.inner.clone();
 
@@ -78,7 +78,6 @@ impl Streamer for StreamX {
             let mut lrx = {
                 let mut ss = ss.lock().await;
                 for i in ss.items.iter() {
-                    println!("initial: {}", i.id);
                     tx.send(Ok(i.as_pb_event())).await.unwrap();
                 }
 
@@ -98,7 +97,7 @@ impl Streamer for StreamX {
                 match tx.send(Ok(n)).await {
                     Ok(_) => {},
                     Err(e) => {
-                        eprintln!("closing listener: {}", e);
+                        //eprintln!("closing listener: {}", e);
                         // implicitly closes our end of the channel
                         return;
                     },
@@ -115,9 +114,9 @@ async fn send_event(ss: &mut Stream,event: ItemEvent) {
     for ix in 0..ss.listeners.len() {
         match ss.listeners[ix].queue.send(event.clone()).await {
             Ok(_) => {},
-            Err(e) => {
+            Err(_) => {
                 // other end closed down, remove this listener
-                println!("closing listener");
+                //println!("closing listener");
                 ss.listeners.remove(ix);
             },
         }
@@ -128,47 +127,68 @@ async fn send_event(ss: &mut Stream,event: ItemEvent) {
 async fn generate(sa: Arc<Mutex<Stream>>) {
     let mut next_id = 1;
 
+    let mut events_total = 0;
+    let mut since_last_print = 0;
+    let mut start_time = tokio::time::Instant::now(); 
+    let mut print_time = start_time;
+
     loop {
-        tokio::time::delay_for(std::time::Duration::from_millis(100)).await;
+        tokio::time::delay_for(std::time::Duration::from_millis(1)).await;
 
         {
-            {
-                let mut ss = sa.lock().await;
-                if ss.items.len() < MAX_ITEMS {
-                    if rand::random() {
-                        println!("adding new item {}", next_id);
-                        // locks?
-                        let i = Item {
-                            id: next_id,
-                            state: true,
-                        };
-                        next_id += 1;
+            let mut sz = sa.lock().await;
+            let ss : &mut Stream = &mut *sz;
 
-                        ss.items.push(i.clone());
+            if ss.items.len() < MAX_ITEMS {
+                if rand::random() {
+                    //println!("adding new item {}", next_id);
+                    // locks?
+                    let i = Item {
+                        id: next_id,
+                        state: true,
+                    };
+                    next_id += 1;
 
-                        send_event(&mut ss, ItemEvent {
-                            id: i.id,
-                            new_state: i.state,
-                        }).await;
-                    }
+                    ss.items.push(i.clone());
+
+                    since_last_print += 1;
+                    events_total += 1;
+                    send_event(ss, ItemEvent {
+                        id: i.id,
+                        new_state: i.state,
+                    }).await;
                 }
             }
 
-            let mut sz = sa.lock().await;
-            let ss : &mut Stream = &mut *sz;
             if ss.items.len() != 0 {
                 let idx = rand::thread_rng().gen_range(0, ss.items.len());
                 let i = &mut ss.items[idx];
                 let new_state = rand::random();
                 if i.state != new_state{
-                    println!("item change: {} to {}", i.id, new_state);
+                    //println!("item change: {} to {}", i.id, new_state);
                     i.state = new_state;
                     let e = ItemEvent {
                         id: i.id,
                         new_state: i.state,
                     };
+                    since_last_print += 1;
+                    events_total += 1;
                     send_event(ss, e).await;
                 }
+            }
+
+            let new_time = tokio::time::Instant::now();
+            let print_duration = tokio::time::Duration::from_secs(1);
+            let time_diff = new_time - print_time;
+            if time_diff > print_duration {
+                let total_time = new_time - start_time;
+                info!("events emitted: {} ({}/s), in last {:?}: {} ({}/s)",
+                    events_total, (events_total / total_time.as_secs()),
+                    time_diff, since_last_print, (since_last_print / time_diff.as_secs())
+                    );
+
+                print_time = new_time;
+                since_last_print = 0;
             }
         }
     }
@@ -176,6 +196,8 @@ async fn generate(sa: Arc<Mutex<Stream>>) {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
     let addr = "[::]:7777".parse().unwrap();
 
     let sx = StreamX::default();
